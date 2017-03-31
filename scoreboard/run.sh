26d9ac0c417ea22dc64ca66ffd79fa21f4800606
sure-fire <sure@fire>
2017-03-30 19:00:15 -0700

    Moved to uwsgi + nginx

--- scoreboard/run.sh
+++ scoreboard/run.sh
@@ -2,4 +2,6 @@ python3 ./wait_for_database.py
 python3 ./manage.py makemigrations challenges
 python3 ./manage.py migrate
 python3 ./manage.py loaddata challenges
-python3 ./manage.py runserver 0.0.0.0:9000
+#python3 ./manage.py runserver 0.0.0.0:9000
+python3 ./manage.py collectstatic --no-input
+uwsgi -s :8001 --module scoreboard.wsgi
