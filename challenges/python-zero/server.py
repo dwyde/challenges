dff2f149a29e64dc9b5dca54ac766ff7f5cd3ab2
David Wyde <david.wyde@gmail.com>
2018-10-29 11:29:00 -0700

    Convert python-zero to use Tornado

--- challenges/python-zero/server.py
+++ challenges/python-zero/server.py
@@ -1,53 +1,101 @@
-"""
-Code heavily based on:
-https://docs.python.org/3/library/asyncio-protocol.html#tcp-echo-server-protocol
-"""
-import asyncio
+import os.path
+import urllib.parse
+import xml.sax.saxutils
+
+import tornado.ioloop
+import tornado.web
 
 import challenge
 
-# The host on which this server will listen.
-HOST = '0.0.0.0'
 
-# The port on which this server will listen.
+# The port on which this application will listen
 PORT = 8888
 
 
-class EchoServerClientProtocol(asyncio.Protocol):
+class MainHandler(tornado.web.RequestHandler):
 
-    def connection_made(self, transport):
-        self.transport = transport
+    def get(self):
+        self.write('\n'.join([
+            '<html>',
+            '<head>',
+            '<style>',
+            '   p {white-space: pre-wrap}',
+            '</style>',
+            '<meta charset="utf-8">',
+            '</head>',
+            '<body>',
+            '<form method="POST">',
+            '   <label for="input">Input:</label>',
+            '   <br>',
+            '   <input id="input" name="input">',
+            '   <br>',
+            '   <input type="submit" value="Submit">',
+            '</form>',
+            '</body>',
+            '</html>',
+        ]))
 
-    def data_received(self, data):
-        message = data.decode().strip()
-
+    def post(self):
+        """ Handle HTTP POST: check the provided name.
+        """
+        user_input = self._read_user_input()
         try:
-            result = challenge.main(message)
+            result = challenge.main(user_input)
         except Exception as ex:
-            result = str(ex)
+            result = 'Error: {}'.format(ex)
+        self._write_result(result)
 
-        output = '{}\n'.format(result).encode()
-        self.transport.write(output)
-        self.transport.close()
+    def _read_user_input(self):
+        """ Parse a value from user-supplied data.
+        """
+        body = self.request.body.decode()
+        cleaned = body.rstrip('\r\n')
+        query = urllib.parse.parse_qs(cleaned)
+        user_input = query.get('input', [''])
+        return user_input[0]
+
+    def _write_result(self, result):
+        """ Write a result to the client.
+        """
+        text_out = str(result)
+        output = xml.sax.saxutils.escape(text_out)
+        self.write(output)
+
+    def set_default_headers(self):
+        """ Do not send an informative Server header.
+        """
+        self.set_header('Server', 'CTF')
+
+    def initialize(self, flag):
+        """ Set up the handler object.
+        """
+        self.flag = flag
+
+
+def read_flag():
+    """ Read in this challenge's flag.
+    """
+    path = os.path.join(os.path.dirname(__file__), 'flag')
+    with open(path) as fp:
+        return fp.read().strip()
+
+
+def make_app():
+    """ Create the Tornado app.
+    """
+    return tornado.web.Application([
+        (r'/', MainHandler, {'flag': read_flag()}),
+    ])
+
 
 def main():
-    loop = asyncio.get_event_loop()
-    # Each client connection will create a new protocol instance
-    coro = loop.create_server(EchoServerClientProtocol, HOST, PORT)
-    server = loop.run_until_complete(coro)
-
-    # Serve requests until Ctrl+C is pressed
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-
-    # Close the server
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
+    """ Run the application.
+    """
+    app = make_app()
+    app.listen(PORT)
+    tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == '__main__':
     main()
+
--- scoreboard/challenges/static/python-zero/challenge.py
+++ scoreboard/challenges/static/python-zero/challenge.py
@@ -54,3 +54,4 @@ if __name__ == '__main__':
    user_input = sys.stdin.readline().strip()
    result = main(user_input)
    print(result)
+
