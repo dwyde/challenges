b565ed1ecba58362438798171977e34918cab95d
David Wyde <david.wyde@gmail.com>
2018-11-13 19:18:05 -0800

    Remove commented line

--- scoreboard/run.sh
+++ scoreboard/run.sh
@@ -2,9 +2,7 @@ python3 ./wait_for_database.py
 python3 ./manage.py makemigrations --no-input
 python3 ./manage.py migrate
 python3 ./manage.py loaddata challenges
-#python3 ./manage.py runserver 0.0.0.0:9000
 python3 ./manage.py collectstatic --no-input
 uwsgi --stop site.sock
 uwsgi -s site.sock --chmod-socket=664 --module scoreboard.wsgi -p 2 \
       --uid scoreboard --master --enable-threads
-
