d623d0cb9909d954f0345d5e7216eff3663619a0
David Wyde <david.wyde@gmail.com>
2018-11-15 18:51:16 -0800

    Use 4 uwsgi workers, not 2

--- scoreboard/run.sh
+++ scoreboard/run.sh
@@ -4,5 +4,5 @@ python3 ./manage.py migrate
 python3 ./manage.py loaddata challenges
 python3 ./manage.py collectstatic --no-input
 uwsgi --stop site.sock
-uwsgi -s site.sock --chmod-socket=664 --module scoreboard.wsgi -p 2 \
+uwsgi -s site.sock --chmod-socket=664 --module scoreboard.wsgi -p 4 \
       --uid scoreboard --master --enable-threads
