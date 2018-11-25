dfd4c86b6e221dc61878328fae64404591f0827d
David Wyde <david.wyde@gmail.com>
2018-11-24 18:00:14 -0800

    Use themed error pages for Nginx 4xx/5xx

---
+++ scoreboard/challenges/error_views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render_to_response
+
+
+def handler404(request, template_name="404.html"):
+    response = render_to_response(template_name)
+    response.status_code = 404
+    return response
+
+
+def handler500(request, template_name="500.html"):
+    response = render_to_response(template_name)
+    response.status_code = 500
+    return response
+
--- scoreboard/scoreboard/urls.py
+++ scoreboard/scoreboard/urls.py
@@ -1,10 +1,13 @@
 """ scoreboard URL Configuration """
 from django.conf.urls import url
 
+from challenges import error_views
 from challenges import views
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^challenges/$', views.list_challenges),
     url(r'^challenges/([-a-z]+)/$', views.show_challenge),
+    url(r'^error/page-not-found/$', error_views.handler404),
+    url(r'^error/server-error/$', error_views.handler500)
 ]
